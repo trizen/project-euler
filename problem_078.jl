999179234f714a4d32b565ab8dedc8a32a699f6f
trizen <trizenx@gmail.com>
2016-08-09 13:23:11 +0300

    new file:   problem_078.jl new file:   problem_078.pl

---
+++ problem_078.jl
@@ -0,0 +1,34 @@
+#!/usr/bin/julia
+
+# Author: Daniel "Trizen" Șuteu
+# License: GPLv3
+# Website: https://github.com/trizen
+
+# https://projecteuler.net/problem=78
+
+function coin_partitions()
+    n = 2
+    p = [1]
+
+    while(true)
+        i = 0
+        q = 2
+        push!(p, 0)
+
+        while q <= n
+            p[n] += (i % 4 > 1 ? -1 : 1) * p[n-q+1]
+            p[n] %= 1000000
+            i += 1
+            j = div(i, 2) + 1
+            isodd(i) && (j *= -1)
+            q = div(j * (3j - 1), 2) + 1
+        end
+
+        p[n] == 0 && break
+        n += 1
+    end
+
+    n - 1
+end
+
+println(coin_partitions())
---
+++ problem_078.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+
+# Author: Daniel "Trizen" Șuteu
+# License: GPLv3
+# Website: https://github.com/trizen
+
+# https://projecteuler.net/problem=78
+
+use 5.010;
+use strict;
+use warnings;
+
+my $n = 1;
+my @p = (1);
+
+while (1) {
+    my $i = 0;
+    my $q = 1;
+    push @p, 0;
+
+    while ($q <= $n) {
+        $p[$n] += ($i % 4 > 1 ? -1 : 1) * $p[$n - $q];
+        $p[$n] %= 1000000;
+        my $j = int(++$i / 2) + 1;
+        if ($i % 2 != 0) {
+            $j *= -1;
+        }
+        $q = int($j * (3 * $j - 1) / 2);
+    }
+
+    last if ($p[$n] == 0);
+    ++$n;
+}
+
+say $n;
