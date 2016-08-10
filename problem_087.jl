72337e0546d72a68369c1acb7e4ee145dfe72bd4
trizen <trizenx@gmail.com>
2016-08-10 17:01:46 +0300

    new file:   problem_087.jl

---
+++ problem_087.jl
@@ -0,0 +1,40 @@
+#!/usr/bin/julia
+
+# Author: Daniel "Trizen" Șuteu
+# License: GPLv3
+# Website: https://github.com/trizen
+
+# https://projecteuler.net/problem=87
+
+limit = 50_000_000
+max = isqrt(limit)
+primes = Int64[2]
+
+for i in 1:2:max
+    isprime(i) && push!(primes, i)
+end
+
+function count_primes(limit)
+
+    seen = Dict{Int64, Bool}()
+    len = length(primes)
+    count = 0
+
+    for p in primes
+        for j in 1:len
+            p^2 + primes[j]^3 < limit || break
+            for k in 1:len
+                n = p^2 + primes[j]^3 + primes[k]^4
+                n < limit || break
+                if !haskey(seen, n)
+                    seen[n] = true
+                    count += 1;
+                end
+            end
+        end
+    end
+
+    count
+end
+
+println(count_primes(limit))
