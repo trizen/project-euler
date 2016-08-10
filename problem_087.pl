f0e2366a6095af6ee6895c01efee29271366c7e6
trizen <trizenx@gmail.com>
2016-08-10 16:47:23 +0300

    new file:   problem_087.pl

---
+++ problem_087.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+# Author: Daniel "Trizen" Șuteu
+# License: GPLv3
+# Website: https://github.com/trizen
+
+# https://projecteuler.net/problem=87
+
+use 5.010;
+use ntheory qw(primes);
+
+my @primes = @{primes(int(sqrt(50e6)))};
+
+my %seen;
+my $end   = $#primes;
+my $count = 0;
+
+foreach my $i (0 .. $end) {
+    foreach my $j (0 .. $end) {
+        $primes[$i]**2 + $primes[$j]**3 < 50e6 or last;
+        foreach my $k (0 .. $end) {
+            (my $n = $primes[$i]**2 + $primes[$j]**3 + $primes[$k]**4) < 50e6 or last;
+            ++$count if !$seen{$n}++;
+        }
+    }
+}
+
+say $count;
