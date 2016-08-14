0711f2fca90f18a44eab886983944023db545d70
trizen <trizenx@gmail.com>
2016-08-15 00:30:49 +0300

    new file:   problem_072.pl

---
+++ problem_072.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+# Author: Daniel "Trizen" Șuteu
+# Date: 15 August 2016
+# License: GPLv3
+# Website: https://github.com/trizen
+
+# https://projecteuler.net/problem=72
+
+use 5.010;
+use ntheory qw(euler_phi);
+
+sub count_frac {
+    my ($n) = @_;
+
+    my $sum = 0;
+    foreach my $k (1 .. $n) {
+        $sum += euler_phi($k);
+    }
+    $sum - 1;
+}
+
+say count_frac(1e6);
