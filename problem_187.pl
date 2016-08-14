bcf35f6fe839f02735f24fe283bd73af5e1587b7
trizen <trizenx@gmail.com>
2016-08-14 15:51:27 +0300

    new file:   problem_187.pl

---
+++ problem_187.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+# Author: Daniel "Trizen" Șuteu
+# License: GPLv3
+# Website: https://github.com/trizen
+
+# How many composite integers, n < 10^8, have precisely two, not necessarily distinct, prime factors?
+
+# https://projecteuler.net/problem=187
+
+use 5.010;
+use strict;
+use warnings;
+
+use ntheory qw(primes);
+
+my $limit = 10**8;
+my $primes = primes(0, $limit / 2);
+
+my $count = 0;
+my $end   = $#{$primes};
+
+foreach my $i (0 .. $end) {
+    foreach my $j ($i .. $end) {
+        $primes->[$i] * $primes->[$j] >= $limit ? last : ++$count;
+    }
+}
+
+say $count;
