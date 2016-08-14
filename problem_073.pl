a6895a04f6a797f9ba1baef5e9151e263a5feef4
trizen <trizenx@gmail.com>
2016-08-15 01:07:07 +0300

    new file:   problem_073.pl new file:   problem_073_comb.pl

---
+++ problem_073.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+# Author: Daniel "Trizen" Șuteu
+# Date: 15 August 2016
+# License: GPLv3
+# Website: https://github.com/trizen
+
+# https://projecteuler.net/problem=73
+
+use 5.010;
+use strict;
+
+use ntheory qw(gcd);
+
+sub count_frac {
+    my ($n, $min, $max) = @_;
+
+    my $count = 0;
+
+    foreach my $n ($min + $max .. $n) {
+        foreach my $k (int($n / $min) + 1 .. int(($n - 1) / $max)) {
+            ++$count if (gcd($n, $k) == 1);
+        }
+    }
+
+    $count;
+}
+
+say count_frac(12_000, 3, 2);
---
+++ problem_073_comb.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+
+# Author: Daniel "Trizen" Șuteu
+# Date: 15 August 2016
+# License: GPLv3
+# Website: https://github.com/trizen
+
+# https://projecteuler.net/problem=73
+
+use 5.010;
+use strict;
+
+use ntheory qw(gcd forcomb);
+
+sub count_frac {
+    my ($n, $min, $max) = @_;
+
+    my $count = 0;
+
+    forcomb {
+        my ($x, $y) = ($_[0] + 1, $_[1] + 1);
+        if ($x / $y > $min and $x / $y < $max and gcd($x, $y) == 1) {
+            ++$count;
+        }
+    } $n, 2;
+
+    $count;
+}
+
+say count_frac(12_000, 1 / 3, 1 / 2);
