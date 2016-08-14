70ce8a16b79be8847ba6c12993d1dc9c46fb1604
trizen <trizenx@gmail.com>
2016-08-15 01:50:58 +0300

    new file:   problem_078_rec.pl

---
+++ problem_078_rec.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+
+# Author: Daniel "Trizen" Șuteu
+# Date: 15 August 2016
+# License: GPLv3
+# Website: https://github.com/trizen
+
+# https://projecteuler.net/problem=78
+
+use 5.010;
+use strict;
+
+use POSIX qw(ceil);
+use Memoize qw(memoize);
+
+memoize('partitions_count');
+
+sub partitions_count {
+    my ($n) = @_;
+
+    return $n if ($n <= 1);
+
+    my $sum_1 = 0;
+    foreach my $i (1 .. (sqrt(24 * $n + 1) + 1) / 6) {
+        $sum_1 += ((-1)**($i - 1) * partitions_count($n - $i * (3 * $i - 1) / 2)) % 1e6;
+    }
+
+    my $sum_2 = 0;
+    foreach my $i (1 .. ceil((sqrt(24 * $n + 1) - 7) / 6)) {
+        $sum_2 += ((-1)**($i - 1) * partitions_count($n - (-$i) * (-3 * $i - 1) / 2)) % 1e6;
+    }
+
+    ($sum_1 + $sum_2) % 1e6;
+}
+
+my $n = 1;
+
+while (1) {
+    if (partitions_count($n) == 0) {
+        say $n- 1;
+        last;
+    }
+    ++$n;
+}
