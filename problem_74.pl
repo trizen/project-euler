c02d4d46f476fb187fb34c31b916a7d77e09e6e8
trizen <trizenx@gmail.com>
2016-08-16 22:36:56 +0300

    new file:   problem_74.pl

---
+++ problem_74.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+
+# Author: Daniel "Trizen" Șuteu
+# Date: 16 August 2016
+# License: GPLv3
+# Website: https://github.com/trizen
+
+# https://projecteuler.net/problem=74
+
+use 5.010;
+use strict;
+
+use ntheory qw(factorial);
+use List::Util qw(sum);
+
+my @factorial = (map { factorial($_) } 0 .. 9);
+
+sub f {
+    my ($n) = @_;
+
+    my %seen = ($n => 1);
+
+    while (1) {
+        my $m = sum(map { $factorial[$_] } split //, $n);
+        exists($seen{$m}) && last;
+        undef $seen{$m};
+        $n = $m;
+    }
+
+    keys(%seen) == 60;
+}
+
+my $count = 0;
+foreach my $i (0 .. 1e6 - 1) {
+    ++$count if f($i);
+}
+
+say $count;
