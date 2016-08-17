f886ef82edd5a3c616ca383d3f48dae5138522f4
trizen <trizenx@gmail.com>
2016-08-17 18:17:49 +0300

    new file:   problem_160.pl

---
+++ problem_160.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+
+# Author: Daniel "Trizen" Șuteu
+# Date: 17 August 2016
+# License: GPLv3
+# Website: https://github.com/trizen
+
+# This is not a very good algorithm!
+# (it takes ~5 hours to complete on my machine)
+
+use 5.010;
+use strict;
+use warnings;
+
+use integer;
+use ntheory qw(forprimes powmod);
+
+sub power {
+    my ($n, $p) = @_;
+
+    my $s = 0;
+    while ($n >= $p) {
+        $s += int($n /= $p);
+    }
+
+    $s;
+}
+
+my $k = 1_000_000_000_000;
+my $t = power($k, 5);
+
+sub f {
+    my ($n) = @_;
+
+    my $p    = 0;
+    my $prod = 1;
+
+    forprimes {
+        if ($p == 1) {
+            $prod *= $_ % 10**5;
+            $prod %= 10**5;
+        }
+        else {
+            $p = power($n, $_);
+
+            if ($_ != 5) {
+                $prod *= powmod($_ % 10**5, ($_ == 2 ? $p - $t : $p), 10**5);
+                $prod %= 10**5;
+            }
+        }
+
+    } $k;
+
+    $prod;
+}
+
+say f($k);
