669d0c83ea3ca776fa4ef4d0c617b0df868d5e64
trizen <trizenx@gmail.com>
2016-08-20 18:46:25 +0300

    new file:   problem_169.pl

---
+++ problem_169.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+# Author: Daniel "Trizen" Șuteu
+# Date: 20 August 2016
+# License: GPLv3
+# Website: https://github.com/trizen
+
+# https://projecteuler.net/problem=169
+
+use 5.010;
+use strict;
+use warnings;
+
+use Memoize qw(memoize);
+use Math::BigNum qw(:constant);
+
+memoize('f');
+
+sub f {
+    my ($n) = @_;
+
+    return 0         if $n == 0;
+    return 1         if $n == 1;
+    return f($n / 2) if $n % 2 == 0;
+
+    f(($n - 1) / 2) + f(($n - 1) / 2 + 1);
+}
+
+say f(10**25 + 1);
