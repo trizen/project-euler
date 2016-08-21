bc7b78d94e79ca2514cde2e463af1917d9a1429e
trizen <trizenx@gmail.com>
2016-08-22 02:48:54 +0300

    modified:   124 Ordered radicals.pl -- optimization: test for square free

--- 124 Ordered radicals.pl
+++ 124 Ordered radicals.pl
@@ -11,18 +11,17 @@ use 5.010;
 use strict;
 use integer;
 
-use ntheory qw(factor);
+use ntheory qw(factor is_square_free);
 use List::Util qw(uniq product);
 
 my @list;
 foreach my $i (1 .. 100000) {
-    push @list, [product(uniq(factor($i))), $i];
+    push @list, [is_square_free($i) ? $i : product(uniq(factor($i))), $i];
 }
 
 @list = sort {
            ($a->[0] <=> $b->[0])
         || ($a->[1] <=> $b->[1])
-    } @list;
-
+} @list;
 
 say $list[10000-1][1];
