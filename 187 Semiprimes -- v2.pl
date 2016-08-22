e8202c16a100eeb45f858767c86a7ad9988f92b9
trizen <trizenx@gmail.com>
2016-08-22 21:36:35 +0300

    new file:   187 Semiprimes -- v2.pl

---
+++ 187 Semiprimes -- v2.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+# Derived from a method posted by Dana Jacobsen.
+# Date: 22 August 2016
+# https://projecteuler.net/problem=187
+
+use strict;
+use integer;
+
+use ntheory qw(prime_count forprimes);
+
+my $limit = 10**8-1;
+my $count = 0;
+
+forprimes {
+    $count += prime_count($_, $limit/$_);
+} int(sqrt($limit));
+
+print "$count\n";
