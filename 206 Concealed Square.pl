ea0fc1cc01583ca4532bbe12e7e2063c5c5e6c9b
trizen <trizenx@gmail.com>
2016-08-24 20:19:42 +0300

    new file:   206 Concealed Square.pl

---
+++ 206 Concealed Square.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+# Author: Daniel "Trizen" Șuteu
+# License: GPLv3
+# Date: 24 August 2016
+# Website: https://github.com/trizen
+
+# https://projecteuler.net/problem=206
+
+use 5.010;
+use strict;
+use warnings;
+
+my $n = int sqrt(1020304050607080900);    # lower-limit
+my $m = int sqrt(1929394959697989990);    # upper-limit
+
+for (my $i = $m + (10 - ($m % 10)) ; $i >= $n ; $i -= 10) {
+    if (($i**2) =~ /^1.2.3.4.5.6.7.8.9.0\z/) {
+        say $i;
+        last;
+    }
+}
