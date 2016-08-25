8760788911446c7cd2c71a4a46765db304b206f6
trizen <trizenx@gmail.com>
2016-08-25 19:21:16 +0300

    new file:   407 Idempotents -- v2.pl new file:   407 Idempotents.pl

---
+++ 407 Idempotents -- v2.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/perl
+
+# Author: Daniel "Trizen" Șuteu
+# Date: 25 August 2016
+# License: GPLv3
+# Website: https://github.com/trizen
+
+# https://projecteuler.net/problem=407
+
+use 5.010;
+use strict;
+use integer;
+
+use ntheory qw(
+  powmod
+  factor
+  forcomb
+  euler_phi
+  prime_count
+  is_prime_power
+  forcomposites
+);
+
+use List::Util qw(uniq product);
+
+my $limit = 10**7;
+my $sum   = prime_count($limit);
+
+forcomposites {
+    if (is_prime_power($_)) {
+        ++$sum;
+    }
+    else {
+        my $c   = $_;
+        my $max = 0;
+
+        my @f   = uniq(factor($c));
+        my $len = scalar(@f);
+
+        foreach my $i (1 .. $len - 1) {
+            forcomb {
+                my $d = product(@f[@_]);
+                my $f = $c / $d;
+                my $g = $d * powmod($d, euler_phi($f) - 1, $f);
+                $g > $max && ($max = $g);
+            } $len, $i;
+        }
+
+        $sum += $max;
+    }
+} $limit;
+
+say $sum;
---
+++ 407 Idempotents.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+
+# Author: Daniel "Trizen" Șuteu
+# Date: 25 August 2016
+# License: GPLv3
+# Website: https://github.com/trizen
+
+# https://projecteuler.net/problem=407
+
+use 5.010;
+use strict;
+use integer;
+
+use ntheory qw(
+  invmod
+  fordivisors
+  prime_count
+  is_prime_power
+  forcomposites
+);
+
+my $limit = 10**7;
+my $sum   = prime_count($limit);
+
+forcomposites {
+    if (is_prime_power($_)) {
+        ++$sum;
+    }
+    else {
+        my $c   = $_;
+        my $max = 0;
+
+        fordivisors {
+            my $g = invmod($_, $c / $_) * $_;
+            $g > $max && ($max = $g);
+        } $c;
+
+        $sum += $max;
+    }
+} $limit;
+
+say $sum;
