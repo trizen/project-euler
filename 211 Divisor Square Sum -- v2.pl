735a096af3a35bf7eb1d74ee07cda1c5d8e1e87f
trizen <trizenx@gmail.com>
2016-08-28 15:32:34 +0300

    new file:   211 Divisor Square Sum -- v2.pl

---
+++ 211 Divisor Square Sum -- v2.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+# Daniel "Trizen" Șuteu
+# Date: 28 August 2016
+# License: GPLv3
+# https://github.com/trizen
+
+# https://projecteuler.net/problem=211
+
+use 5.010;
+use strict;
+
+use LWP::Simple qw(get);
+
+my $url   = 'http://oeis.org/A046655/b046655.txt';
+my $limit = 64_000_000;
+
+my $sum = 0;
+
+foreach my $term (split(/\R/, get($url))) {
+    my (undef, $n) = split(' ', $term);
+    last if $n >= $limit;
+    $sum += $n;
+}
+
+say $sum;
