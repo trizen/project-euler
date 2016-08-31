23ab5e7a9f066afa777acbbf664ea2011acd845a
trizen <trizenx@gmail.com>
2016-08-31 18:05:14 +0300

    modified:   064 Odd period square roots.pl

--- 064 Odd period square roots.pl
+++ 064 Odd period square roots.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 # Daniel "Trizen" Șuteu
-# Date: 21 August 2016
+# Date: 31 August 2016
 # License: GPLv3
 # https://github.com/trizen
 
@@ -18,20 +18,17 @@ use ntheory qw(is_power sqrtint);
 sub period_length {
     my ($n) = @_;
 
-    my $x_0 = sqrtint($n);
-    my $y   = $x_0;
-    my $z   = $n - $x_0 * $x_0;
-
-    my $y_0 = $y;
-    my $z_0 = $z;
+    my $x = sqrtint($n);
+    my $y = $x;
+    my $z = 1;
 
     my $period = 0;
 
     do {
-        $y = int(($x_0 + $y) / $z) * $z - $y;
+        $y = int(($x + $y) / $z) * $z - $y;
         $z = int(($n - $y * $y) / $z);
         ++$period;
-    } until (($y == $y_0) && ($z == $z_0));
+    } until (($y == $x) && ($z == 1));
 
     $period;
 }
