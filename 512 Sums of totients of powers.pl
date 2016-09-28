eb7a8fb92ef10037917ea6f6a7e48e81f3352ee9
trizen <trizenx@gmail.com>
2016-09-28 22:14:06 +0300

    new file:   512 Sums of totients of powers.pl

---
+++ 512 Sums of totients of powers.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+# Daniel "Trizen" Șuteu
+# License: GPLv3
+# Date: 28 September 2016
+# Website: https://github.com/trizen
+
+# https://projecteuler.net/problem=512
+
+# Runtime: 5 min 02.77s
+
+use 5.010;
+use strict;
+use integer;
+use ntheory qw(euler_phi);
+
+sub g {
+    my ($n) = @_;
+
+    my $sum = 0;
+    for (my $k = 1 ; $k <= $n ; $k += 2) {
+        $sum += euler_phi($k) % ($k + 1);
+    }
+    $sum;
+}
+
+say g(5 * 10**8);
