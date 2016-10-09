6800952ad7c531fd3eba11268be8067ce3a0f059
trizen <trizenx@gmail.com>
2016-10-09 22:13:01 +0300

    new file:   095 Amicable chains.pl

---
+++ 095 Amicable chains.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+
+# Daniel "Trizen" Șuteu
+# License: GPLv3
+# Date: 15 September 2016
+# Website: https://github.com/trizen
+
+# https://projecteuler.net/problem=95
+
+# Runtime: 7.517s
+
+use 5.010;
+use strict;
+use integer;
+
+use ntheory qw(divisor_sum);
+
+my $limit = 1e6;
+
+my %chain;
+foreach my $n (1 .. $limit) {
+    my $len  = 0;
+    my $orig = $n;
+
+    my %seen;
+    while (1) {
+        my $sum = divisor_sum($n) - $n;
+        last if $seen{$sum}++;
+        last if $sum > $limit;
+        $n = $sum;
+        ++$len;
+    }
+
+    if (exists $seen{$orig}) {
+        $chain{$len} //= $orig;
+    }
+}
+
+say $chain{(sort { $b <=> $a } keys %chain)[0]};
