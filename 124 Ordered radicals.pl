e0ea6474c195952c8aa27582cfd86a8b16be58fe
trizen <trizenx@gmail.com>
2017-01-19 14:14:22 +0200

    modified:   124 Ordered radicals.pl modified:   124 Ordered radicals.sf

--- 124 Ordered radicals.pl
+++ 124 Ordered radicals.pl
@@ -7,18 +7,22 @@
 
 # https://projecteuler.net/problem=124
 
-# Runtime: 0.314s
+# Runtime: 0.244s
 
 use 5.010;
 use strict;
 use integer;
 
-use ntheory qw(factor is_square_free);
-use List::Util qw(uniq product);
+use ntheory qw(factor_exp is_square_free vecprod);
 
 my @list;
 foreach my $i (1 .. 100000) {
-    push @list, [is_square_free($i) ? $i : product(uniq(factor($i))), $i];
+    push @list, [
+        is_square_free($i)
+            ? $i
+            : vecprod(map $_->[0], factor_exp($i)),
+        $i,
+    ];
 }
 
 @list = sort {
@@ -26,4 +30,4 @@ foreach my $i (1 .. 100000) {
         || ($a->[1] <=> $b->[1])
 } @list;
 
-say $list[10000-1][1];
+say $list[10000 - 1][1];
