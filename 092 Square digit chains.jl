a604f5db7978c83f5daeba4fd294103440d429fa
trizen <trizenx@gmail.com>
2017-01-30 17:53:20 +0200

    new file:   092 Square digit chains.jl

---
+++ 092 Square digit chains.jl
@@ -0,0 +1,35 @@
+#!/usr/bin/julia
+
+# Daniel "Trizen" Șuteu
+# License: GPLv3
+# Date: 30 January 2017
+# https://github.com/trizen
+
+# https://projecteuler.net/problem=92
+
+# Runtime: 3.236s
+
+const cache = zeros(Int64, 10^7)
+
+cache[ 1] = 1
+cache[89] = 89
+
+function chain(n::Int64)
+    if (cache[n] == 0)
+        cache[n] = chain(sum(map((d)->d^2, digits(n))))
+    else
+        cache[n]
+    end
+end
+
+function count()
+    c = 0
+    for n in 1:10^7
+        if (chain(n) == 89)
+            c += 1
+        end
+    end
+    return c
+end
+
+println(count())
