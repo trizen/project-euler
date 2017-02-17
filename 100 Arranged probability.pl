9d7e8b146053f50fc4a286bb359f1dde6d81e5b6
trizen <trizenx@gmail.com>
2017-02-17 19:30:34 +0200

    new file:   100 Arranged probability.pl

---
+++ 100 Arranged probability.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+
+# Author: Daniel "Trizen" Șuteu
+# Date: 16 August 2016
+# License: GPLv3
+# Website: https://github.com/trizen
+
+# https://projecteuler.net/problem=100
+
+# Runtime: 0.004s
+
+use strict;
+use warnings;
+
+for (my $m = 1 ; ; ++$m) {
+
+    my $n = 1/4 * (
+        -(1 + sqrt(2)) * (3 - 2*sqrt(2))**$m
+        -(1 - sqrt(2)) * (3 + 2*sqrt(2))**$m - 2
+    );
+
+    if ($n > 10**12) {
+
+        my $b = 1/8 * (
+            (sqrt(2) + 2) * (3 - 2*sqrt(2))**$m
+          - (sqrt(2) - 2) * (3 + 2*sqrt(2))**$m + 4
+        );
+
+        printf("%.0f\n", $b);
+        last;
+    }
+}
