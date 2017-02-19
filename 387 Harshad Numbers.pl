84f0b3590a84a89eebb80738779d2b650a1dabc3
trizen <trizenx@gmail.com>
2017-02-20 00:55:12 +0200

    new file:   387 Harshad Numbers.pl

---
+++ 387 Harshad Numbers.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl
+
+# Daniel "Trizen" Șuteu
+# Date: 20 February 2017
+# License: GPLv3
+# https://github.com/trizen
+
+# https://projecteuler.net/problem=387
+
+# Runtime: 0.095s
+
+use 5.010;
+use strict;
+use warnings;
+
+use ntheory qw(:all);
+
+my @valid;
+my $limit = 10**14;
+
+sub recurse {
+    my ($num, $sum) = @_;
+
+    if (($num . '01') >= $limit) {
+        return;
+    }
+
+    foreach my $n (0 .. 9) {
+
+        my $x = $num . $n;
+        my $y = $sum + $n;
+
+        if ($x % $y == 0) {
+            foreach my $i (1, 3, 7, 9) {
+                if (($x . $i) < $limit and is_prime($x . $i)) {
+                    push(@valid, $x . $i) if is_prime($x / $y);
+                }
+            }
+
+            recurse($x, $y);
+        }
+    }
+}
+
+recurse($_, $_) for (1, 2, 4, 6, 8);
+
+say vecsum(@valid);
