c9eb9ed064aafd2e02f67fbf1ed056bcd05efbce
trizen <trizenx@gmail.com>
2017-02-20 21:58:30 +0200

    new file:   348 Sum of a square and a cube.pl

---
+++ 348 Sum of a square and a cube.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+
+# Daniel "Trizen" Șuteu
+# Date: 20 February 2017
+# License: GPLv3
+# https://github.com/trizen
+
+# https://projecteuler.net/problem=348
+
+# Runtime: 2 min, 52 sec
+
+use 5.010;
+use strict;
+use integer;
+
+my %count;
+my $count = 0;
+my $sum   = 0;
+
+OUTER: for (my $i = 1 ; ; ++$i) {
+    for (my $j = 1 ; ; ++$j) {
+
+        my $s = $i**2;
+        my $c = $j**3;
+        my $p = $s + $c;
+
+        if ($p eq reverse($p)) {
+            push @{$count{$p}}, "$i^2 + $j^3";
+            if (@{$count{$p}} == 4) {
+                $sum += $p;
+                say "$p = [", join(", ", @{$count{$p}}), "]";
+                last OUTER if ++$count == 5;
+            }
+        }
+
+        last if $j > $i;
+    }
+}
+
+say "Sum: $sum";
