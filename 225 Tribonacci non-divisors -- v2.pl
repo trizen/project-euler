1f32884faacb80a3f044dc3ee299572cbb56d139
trizen <trizenx@gmail.com>
2017-05-07 01:37:09 +0300

    modified:   225 Tribonacci non-divisors -- v2.pl

--- 225 Tribonacci non-divisors -- v2.pl
+++ 225 Tribonacci non-divisors -- v2.pl
@@ -7,43 +7,35 @@
 
 # https://projecteuler.net/problem=225
 
-# Runtime: 40.531s
+# Runtime: 11.433s
 
 use 5.010;
 use strict;
 use integer;
 
-use Memoize qw(memoize);
-
 sub tribonacci {
-    my ($n, $k) = @_;
-    $n <= 3 ? 1 : (
-        tribonacci($n - 1, $k) % $k
-      + tribonacci($n - 2, $k) % $k
-      + tribonacci($n - 3, $k) % $k
+    my ($n, $k, $c) = @_;
+    $c->{$n} //= $n <= 3 ? 1 : (
+        tribonacci($n - 1, $k, $c) % $k
+      + tribonacci($n - 2, $k, $c) % $k
+      + tribonacci($n - 3, $k, $c) % $k
     ) % $k;
 }
-
-memoize('tribonacci');
 
 my $num = 0;
 my $nth = 124;
 
-OUTER:
-for (my ($c, $k) = (1, 1) ; ; $k += 2) {
+for (my ($c, $k) = (1, 1) ; $c <= $nth ; $k += 2) {
+    my %cache;
     for (my $n = 4 ; ; $n += 3) {
-        my $t1 = tribonacci($n,     $k);
-        my $t2 = tribonacci($n + 1, $k);
-        my $t3 = tribonacci($n + 2, $k);
+        my $t1 = tribonacci($n,     $k, \%cache) || last;
+        my $t2 = tribonacci($n + 1, $k, \%cache) || last;
+        my $t3 = tribonacci($n + 2, $k, \%cache) || last;
 
         if ($t1 == 1 and $t2 == 1 and $t3 == 1) {
             say "$c -> $k";
             $num = $k;
-            last OUTER if ($c++ == $nth);
-            last;
-        }
-
-        if ($t1 == 0 or $t2 == 0 or $t3 == 0) {
+            $c += 1;
             last;
         }
     }
