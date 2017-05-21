1cd5479902baeaa687c1aaafbd4280f454b3a2b0
trizen <trizenx@gmail.com>
2017-05-21 11:19:42 +0300

    new file:   "174 Counting the number of \"hollow\" square laminae that can form one, two, three, ... distinct arrangements.pl"

---
+++ 174 Counting the number of "hollow" square laminae that can form one, two, three, ... distinct arrangements.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+
+# Daniel "Trizen" Șuteu
+# License: GPLv3
+# Date: 20 May 2017
+# https://github.com/trizen
+
+# https://projecteuler.net/problem=174
+
+# Runtime: 1.050s
+
+use 5.010;
+use strict;
+use warnings;
+
+my %counts;
+my $tiles = 1000000;
+
+foreach my $k (1 .. $tiles >> 2) {
+    for (my ($sum, $j) = (0, $k + 2) ; ; $j += 2) {
+        $sum += 2 * $j + 2 * ($j - 2);
+        $sum <= $tiles ? ++$counts{$sum} : last;
+    }
+}
+
+my $count = 0;
+foreach my $value (values %counts) {
+    if ($value >= 1 and $value <= 10) {
+        ++$count;
+    }
+}
+say $count;
