85278e89c80cb0f9bd67c3939ac33d5302c08bba
trizen <trizenx@gmail.com>
2017-08-20 16:12:54 +0300

    new file:   571 Super Pandigital Numbers.pl

---
+++ 571 Super Pandigital Numbers.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl
+
+# Daniel "Trizen" Șuteu
+# Date: 20 August 2017
+# https://github.com/trizen
+
+# https://projecteuler.net/problem=571
+
+# Brute-force solution.
+
+# Runtime: ~20 min
+
+use 5.010;
+use strict;
+use warnings;
+
+use List::Util qw(uniq all min);
+use ntheory qw(todigits fromdigits);
+use Algorithm::Combinatorics qw(variations);
+
+my $base = shift(@ARGV) // 12;    # pandigital in all bases 2..$base
+my $first = 10;                   # generate first n numbers
+
+my @digits = (
+               1, 0,
+               (2 .. min($base - 1, 9)),
+               ($base > 10
+                 ? ('a' .. chr(ord('a') + $base - 10 - 1))
+                 : ()
+               )
+             );
+
+my @bases = reverse(2 .. $base - 1);
+
+my $sum = 0;
+my $iter = variations(\@digits, $base);
+
+while (defined(my $t = $iter->next)) {
+
+    if ($t->[0] ne '0') {
+        my $n = join('', @$t);
+        my $d = fromdigits($n, $base);
+
+        if (all { uniq(todigits($d, $_)) == $_ } @bases) {
+            say "Found: $n -> $d";
+            $sum += $d;
+            last if --$first == 0;
+        }
+    }
+}
+
+say "Sum: $sum";
