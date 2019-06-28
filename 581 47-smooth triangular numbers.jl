32ebf9991b8a20a0305050137c715f6c183ce801
trizen <614513+trizen@users.noreply.github.com>
2019-06-28 08:26:27 +0300

    modified:   581 47-smooth triangular numbers.jl modified:   581 47-smooth triangular numbers.pl modified:   581 47-smooth triangular numbers.sf

--- 581 47-smooth triangular numbers.jl
+++ 581 47-smooth triangular numbers.jl
@@ -3,47 +3,57 @@
 # Daniel "Trizen" Șuteu
 # License: GPLv3
 # Date: 02 May 2017
+# Edit: 28 June 2019
 # https://github.com/trizen
 
 # https://projecteuler.net/problem=581
 
-# Runtime: ~8 seconds.
+# Runtime: 4.153s.
 
 using Primes
 
 function p_581()
-    smooth = 47
-    prime_nums = primes(smooth)
 
-    function is_smooth(n)
-        for p in prime_nums
-            while (rem(n, p) == 0)
-                n = div(n, p)
+    smooth = 47
+    limit  = 1109496723125       # A002072(π(47)) = A002072(15)
+
+    prime_nums = primes(smooth)
+    smooth_prod = prod(prime_nums)
+
+    function is_smooth_over_prod(n,k)
+
+        g = gcd(n, k)
+
+        while (g > 1)
+            while (rem(n, g) == 0)
+                n = div(n, g)
             end
+            n == 1 && return true
+            g = gcd(n, k)
         end
 
         n == 1
     end
 
-    seen = Dict{Int64,Bool}()
     smooth_nums = Int64[1]
-
-    sum = 0
 
     for p in prime_nums
         for n in smooth_nums
-            if (!haskey(seen, n) && is_smooth(n+1))
-                seen[n] = true
-                sum += n
-            end
-
-            if (n*p <= 1109496723125)
+            if (n*p <= limit)
                 append!(smooth_nums, n*p)
             end
         end
     end
 
-    return sum
+    total = 0
+
+    for n in (smooth_nums)
+        if (is_smooth_over_prod(n+1, smooth_prod))
+            total += n
+        end
+    end
+
+    return total
 end
 
 println(p_581())
--- 581 47-smooth triangular numbers.pl
+++ 581 47-smooth triangular numbers.pl
@@ -3,61 +3,65 @@
 # Daniel "Trizen" Șuteu
 # License: GPLv3
 # Date: 01 May 2017
+# Edit: 28 June 2019
 # https://github.com/trizen
 
 # https://projecteuler.net/problem=581
 
-# Runtime: 1 min.
+# Runtime: 13.415s.
 
 =for comment
 
-Maximum value of n in the p-smooth T(n), is given by: A002072(prime_count(p)).
+Maximum value of n in the p-smooth T(n), is given by A002072(π(p)).
 
 For p=47, the maximum value of n is 1109496723125.
 
 Using the same idea from problem 204, we generate 47-smooth numbers not exceeding this limit.
-Then for each 47-smooth number n, we check n+1 to see if is also 47-smooth.
+Then for each 47-smooth number n, we check n+1 to see if it is also 47-smooth.
 
 =cut
 
-use 5.010;
+use 5.020;
 use strict;
 use warnings;
 
-use ntheory qw(valuation primes);
+use ntheory qw(:all);
+use experimental qw(signatures);
 
-my $smooth = 47;
-my @primes = @{primes($smooth)};
+my $smooth      = 47;
+my $limit       = 1109496723125;        # A002072(π(47)) = A002072(15)
+my @primes      = @{primes($smooth)};
+my $smooth_prod = vecprod(@primes);
 
-sub is_smooth {
-    my ($n) = @_;
+sub is_smooth_over_prod ($n, $k) {
 
-    foreach my $p (@primes) {
-        if ($n % $p == 0) {
-            $n /= $p**valuation($n, $p);
-            return 1 if $n == 1;
-        }
+    my $g = gcd($n, $k);
+
+    while ($g > 1) {
+        $n /= $g while ($n % $g == 0);
+        return 1 if ($n == 1);
+        $g = gcd($n, $k);
     }
 
     $n == 1;
 }
 
-my %seen;
-my $sum    = 0;
 my @smooth = (1);
 
 foreach my $p (@primes) {
     foreach my $n (@smooth) {
-        if (!exists($seen{$n}) and is_smooth($n+1)) {
-            undef $seen{$n};
-            $sum += $n;
-        }
-
-        if ($n*$p <= 1109496723125) {
-            push @smooth, $n*$p;
+        if ($n * $p <= $limit) {
+            push @smooth, $n * $p;
         }
     }
-    say "$p -> $sum";
 }
 
-say "Sum: $sum";
+my $sum = 0;
+
+foreach my $n (@smooth) {
+    if (is_smooth_over_prod($n+1, $smooth_prod)) {
+        $sum += $n;
+    }
+}
+
+say $sum;
