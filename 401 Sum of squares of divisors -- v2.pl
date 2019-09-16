bde1d94d586f67fde3baa44930a511660dfaebe1
trizen <trizen@protonmail.com>
2019-09-16 23:17:50 +0300

    new file:   401 Sum of squares of divisors -- v2.pl modified:   401 Sum of squares of divisors.pl

--- 401 Sum of squares of divisors -- v2.pl
+++ 401 Sum of squares of divisors -- v2.pl
@@ -1,8 +1,7 @@
 #!/usr/bin/perl
 
 # Daniel "Trizen" Șuteu
-# Date: 20 August 2017
-# Edit: 16 September 2019
+# Date: 16 September 2019
 # https://github.com/trizen
 
 # https://projecteuler.net/problem=401
@@ -10,7 +9,7 @@
 # Generalized aglorithm described at:
 #   https://trizenx.blogspot.com/2018/11/partial-sums-of-arithmetical-functions.html
 
-# Runtime: ~47 seconds.
+# Runtime: ~39 seconds.
 
 use 5.010;
 use strict;
@@ -27,25 +26,20 @@ sub f ($n) {
     divmod(mulmod(mulmod($n, $n + 1, $mod), 2 * $n + 1, $mod), 3, $mod) >> 1;
 }
 
-sub sum_of_sum_of_squared_divisors ($n) {
+sub sum_of_sum_of_squared_divisors ($n) {      # using the "Dirichlet hyperbola method"
 
     my $s = sqrtint($n);
-    my $u = int($n/($s+1));
 
-    my $sum = 0;
-    my $prev = f($n);
+    my $total = 0;
 
-    foreach my $k (1 .. $s) {
-        my $curr = f(int($n/($k+1)));
-        $sum  = addmod($sum, mulmod($k, $prev - $curr, $mod), $mod);
-        $prev = $curr;
+    for my $k (1 .. $s) {
+        $total = addmod($total, f(int($n/$k)), $mod);
+        $total = addmod($total, mulmod($k*$k, int($n/$k), $mod), $mod);
     }
 
-    foreach my $k (1 .. $u) {
-        $sum = addmod($sum, mulmod($k*$k, int($n/$k), $mod), $mod);
-    }
+    $total -= mulmod($s, f($s), $mod);
 
-    return $sum;
+    return $total % $mod;
 }
 
 say sum_of_sum_of_squared_divisors($lim);
